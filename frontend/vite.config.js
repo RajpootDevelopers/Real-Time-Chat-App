573ab93a9bf55702b5d15c5a6d21db73a19f9ec9
Afaq Ahmad Rajpoot <ranag55db@gmail.com>
2024-03-20 00:41:31 +0500

    updating vit.confing url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server : {
     port : 3000,
     proxy : {
-      "/api" : { target : "http://localhost:5000"}
+      "/api" : { target : "https://real-time-chat-app-iota-eight.vercel.app/"}
     }
   }
 })
