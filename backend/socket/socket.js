bc4f5faa329ad332aca62217f08b873cc7fbcd9e
Afaq Ahmad Rajpoot <ranag55db@gmail.com>
2024-03-21 00:13:10 +0500

    by mistake i changed in socket.io url now i am chanigin context

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://real-time-chat-app-production-b17c.up.railway.app/"],
+		origin: ["http://localhost:8000"],
 		methods: ["GET", "POST"],
 	},
 });
